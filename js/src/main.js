9698f01262375b267190fb0827b023caa5bd40a2
James Pederson <james@jpederson.com>
2019-05-19 15:22:15 -0500

    Move variables to the sections where they're used, and adjust a few comments.

--- js/src/main.js
+++ js/src/main.js
@@ -3,27 +3,26 @@
 // onload responsive footer and menu stuff
 jQuery(document).ready(function($){
 
-	// select some things we'll use to make things responsive
-	var menu = $( 'header nav' ),
-		menu_toggle = menu.find( 'button.menu-toggle' ),
-		menu_ul = menu.find( '.nav-menu' ),
-		fluid_images = $( '.content img' );
-
 
 	// remove height and width from images inside
+	var fluid_images = $( '.content img' );
 	fluid_images.removeAttr( 'width' ).removeAttr( 'height' );
 
 
 	// show/hide menus when they click the toggler
+	var menu = $( 'header nav' );
+	var menu_toggle = menu.find( 'button.menu-toggle' );
+	var menu_ul = menu.find( '.nav-menu' );
 	menu_toggle.click(function(){
 
+		// if the menu is visible, hide it, 
 		if ( menu_ul.is( ':visible' ) ) {
 			menu_ul.hide();
 		} else {
 			menu_ul.show();
 		}
 
-		// when user clicks a link, open submenu if it exists.
+		// when user clicks a link in the menu, open submenu if it exists.
 		menu_ul.find( 'a' ).click(function(){
 			var parent_li = $( this ).parent( 'li' );
 			var submenu = $( this ).next( 'ul' );
@@ -37,9 +36,10 @@ jQuery(document).ready(function($){
 	});
 
 
-	// open magnific-popup when people click a certain class.
+	// couple of quick bindings for magnific popup
 	$( '.lightbox-iframe' ).magnificPopup({ 'type': 'iframe' });
 	$( '.lightbox' ).magnificPopup({ 'type': 'image' });
+
 
 });
 
